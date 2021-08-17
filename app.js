9e10e2ea9a224d21ece0b9eb0986f1d5bca46b90
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-18 01:22:21 +0530

    added ejs tempelates

--- app.js
+++ app.js
@@ -43,6 +43,9 @@ app.get("/work", function (req, res) {
     res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
+app.get("/about", function (req, res) {
+    res.render("about");
+});
 
 app.listen(3000, function (req, res) {
     console.log("Server started at 3000")
