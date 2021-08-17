8326436ecffd86e780233e2449fe9577d69e5b74
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-17 22:26:38 +0530

    changed the date format(stackoverflow)

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
+
+
+    // Getting the Date
     var today = new Date();
-    var currentDay = today.getDay();
-    day = ""
-
-
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = today.toLocaleDateString("en-US", options)
 
     res.render("list", { kindofDay: day });
 });
